10c79611dcbc2423f1f89eb0587345def24b4e96
Joshua Luppes <lupjos@dunwoody.edu>
2025-10-30 09:14:14 -0500

    Add solution for 1

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,30 @@
 console.log("Script loaded!");
 
-
-function moveTheThing(){
-    // Your code here
+function moveTheThing() {
+  const theThing = document.getElementById("theThing");
+  const rightBlock = document.getElementById("rightBlock");
+  const leftBlock = document.getElementById("leftBlock");
+  leftBlock.childElementCount > 0
+    ? rightBlock.append(theThing)
+    : leftBlock.append(theThing);
 }
 
-function styleTheText(){
-    // Your code here
+function styleTheText() {
+  // Your code here
 }
 
-function getFormValues(){
-    // Your code here
+function getFormValues() {
+  // Your code here
 }
 
-function countTheStuff(){
-    // Your code here
+function countTheStuff() {
+  // Your code here
 }
 
-function addNewRow(){
-    // Your code here
+function addNewRow() {
+  // Your code here
 }
 
-function yourBonusChallenge(){
-    // Your code here
+function yourBonusChallenge() {
+  // Your code here
 }
