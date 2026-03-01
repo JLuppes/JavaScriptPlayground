61655ae0abba97096b019cf3ebc6937a2a1fa805
JJ Jiang <jiayuj@dunwoody.edu>
2026-02-28 21:32:18 -0600

    completed JSPlayground assignment with bonus challenge

--- scripts/script.js
+++ scripts/script.js
@@ -2,25 +2,72 @@ console.log("Script loaded!");
 
 
 function moveTheThing(){
-    // Your code here
+    const cakePic = document.getElementById("theThing")
+    const leftBox = document.getElementById("leftBlock")
+    const rightBox = document.getElementById("rightBlock")
+    if (cakePic.parentElement === leftBox){
+        rightBox.append(cakePic);
+    } else {
+        leftBox.append(cakePic)
+    }
 }
 
 function styleTheText(){
-    // Your code here
+    const theText = document.getElementById("fancyText")
+    theText.style.color = "purple";
+    theText.style.fontFamily = "fantasy";
+    theText.style.fontSize = "30px"
 }
 
 function getFormValues(){
-    // Your code here
+    const firstInput = document.getElementById("firstField")
+    const firstValue = firstInput.value
+    const firstResult = document.getElementById("firstResult")
+    firstResult.textContent = firstValue
+    
+    const secondInput = document.getElementById("secondField")
+    const secondValue = secondInput.value
+    const secondResult = document.getElementById("secondResult")
+    secondResult.textContent = secondValue
+
+    const thirdInput = document.getElementById("thirdField")
+    const thirdValue = thirdInput.checked
+    const thirdResult = document.getElementById("thirdResult")
+    thirdResult.textContent = thirdValue
 }
 
 function countTheStuff(){
-    // Your code here
+    const allP = document.getElementsByTagName("p")
+    const resultP = document.getElementById("countOfP")
+    resultP.textContent = allP.length
+    
+    const allOfHTwo = document.getElementsByTagName("h2");
+    const resultOfHTwo = document.getElementById("countOfH2")
+    resultOfHTwo.textContent = allOfHTwo.length
+
+    const resultTD = document.getElementById("countOfTD")
+    const allTD = document.getElementsByTagName("td")
+    resultTD.textContent = allTD.length
 }
 
+let count = 3
 function addNewRow(){
-    // Your code here
+    const allTable = document.getElementById("addRowsTable")
+    const newRow = document.createElement("tr")
+    const newBlock = document.createElement("td")
+    // count = 3
+    newBlock.innerHTML = ++count
+    newRow.append(newBlock)
+    allTable.append(newRow)
 }
 
 function yourBonusChallenge(){
-    // Your code here
+    const chickenCheck = document.getElementById("chickenAsk")
+    const chickenImage = document.getElementById("bbqChicken")
+    
+    if (chickenCheck.value.toLowerCase() === "barbecue chicken alert") {
+        chickenImage.style.display = "block";
+    } else {
+        chickenImage.style.display = "none";
+    }
 }
